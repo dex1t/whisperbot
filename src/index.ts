43b1251247b43566461e01c10983dea27a284b97
dex1t <dex1t@deg.ooo>
2020-01-17 08:06:08 +0900

    Migrate ts

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+app.receiver.app.get("/", (req, res) => {
+  res.sendStatus(200);
+  console.log("got webhook");
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log("⚡️ Bolt app is running!");
+})();
