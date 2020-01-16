dcb8b435d37d22585101a8e59f09cf221c952b38
dex1t <dex1t@deg.ooo>
2020-01-17 08:23:49 +0900

    Tweak

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import echo from "./features/command";
 
 (async () => {
   const server = await app.start(process.env.PORT || 3000);
-  console.log(`⚡️ Bolt app is running! PORT: ${server.address().port}`);
+  console.log(`⚡️ Bolt app is running!`);
 })();
 
 echo();
