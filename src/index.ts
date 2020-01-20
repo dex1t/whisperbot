7ecbd2e391b395bf0208c4c0762d2c63f8849e05
dex1t <dex1t@deg.ooo>
2020-01-20 19:02:15 +0900

    Tweak

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,22 @@ import { Store } from "./store";
 import slashCommands from "./features/slashCommands";
 import webhookEndpoints from "./features/webhookEndpoints";
 
+// Setup redis
+export const store = new Store({
+  port: 6379,
+  host: process.env.REDIS_HOST
+});
+
+// Setup intercom-client
 const Intercom = require("intercom-client");
 export const intercomClient = new Intercom.Client({
   token: process.env.INTERCOM_ACCESS_TOKEN
 });
 
+// Setup webhook receiver
 export const receiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
-
 const bodyParser = require("body-parser");
 const xhub = require("express-x-hub");
 receiver.app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,15 +27,11 @@ receiver.app.use(
 );
 receiver.app.use(bodyParser.json());
 
+// Setup bot, and boot
 export const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   receiver
-});
-
-export const store = new Store({
-  port: 6379,
-  host: process.env.REDIS_HOST
 });
 
 (async () => {
